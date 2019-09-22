734e7d39334841bd030c61aa3a1779ebe1048558
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-09-22 03:11:59 -0400

    bio js

---
+++ js/modules/dataObject.js
@@ -0,0 +1,29 @@
+export default {
+    sandra: {
+        avatar: "sandra_2.jpg",
+        name: "Sandra Tsao",
+        role: "WEB DEVELOPER & VIDEO EDITOR",
+        bio: "some text"
+    },
+
+    mariam: {
+        avatar: "mariam_2.jpg",
+        name: "Mariam Khalifa",
+        role: "WEB DEVELOPER",
+        bio: "some text"
+    },
+
+    kahani: {
+        avatar: "kahani_2.jpg",
+        name: "Kahani Gajjar",
+        role: "UI & UX DESIGNER",
+        bio: "some text"
+    },
+
+    luisa: {
+        avatar: "luisa_2.jpg",
+        name: "Luisa Valero",
+        role: "BAND, UI & UX DESIGNER",
+        bio: "some text"
+    }
+}
