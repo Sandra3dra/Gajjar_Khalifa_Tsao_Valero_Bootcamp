b7fbb6d085c645b6e059886d938cecb1ea71351f
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-09-22 14:00:58 -0400

    member bio event working

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ import Team from './modules/dataObject.js';
 
     // console.log(Team);
 
-    let memberBio =  document.querySelector('.memberBio').children,  
-        teamImages = document.querySelectorAll('.teamImg');
+    let memberBio =  document.querySelectorAll('.memberBio'),  
+        teamImages = document.querySelectorAll('.teamImg'),
         sandraImg = document.querySelector('#sandraImg'),
         mariamImg = document.querySelector('#mariamImg'),
         kahaniImg = document.querySelector('#kahaniImg'),
@@ -16,61 +16,68 @@ import Team from './modules/dataObject.js';
         luisaBio = document.querySelector('#luisaBio').children;    // console.log(memberBio);
     // console.log(teamImages);
 
-    function parseMember() {
-        // console.log('workin'); 
+    // function parseMember() {
+    //     // console.log('workin'); 
 
-        let firstName = this.alt.split(' '),
-             member = firstName[0];  
-        // console.log(member);
-        memberBio[0].src = `images/${Team[member].avatar}`;
-        memberBio[1].textContent = Team[member].role;
-        memberBio[2].textContent = Team[member].name;
-        memberBio[3].textContent = Team[member].bio;
+    //     let firstName = this.alt.split(' '),
+    //          member = firstName[0];  
+    //     // console.log(member);
+    //     memberBio[0].src = `images/${Team[member].avatar}`;
+    //     memberBio[1].textContent = Team[member].role;
+    //     memberBio[2].textContent = Team[member].name;
+    //     memberBio[3].textContent = Team[member].bio;
 
-    }
+    // }
 
     function parseSandra() {
         
         let firstName = this.alt.split(' '),
-             member = firstName[0];  
+            member = firstName[0];  
+        sandraImg.src = "images/sandra_hover3.jpg";
+        // console.log(this.nextElementSibling);
+        this.nextElementSibling.style.display = 'block';
+        // this.nextSibling.classList.style.display = "block";
         
-        sandraBio[0].src = `images/${Team[member].avatar}`;
-        sandraBio[1].textContent = Team[member].role;
-        sandraBio[2].textContent = Team[member].name;
-        sandraBio[3].textContent = Team[member].bio;
+        // sandraBio[0].src = `images/${Team[member].avatar}`;
+        sandraBio[0].textContent = Team[member].role;
+        sandraBio[1].textContent = Team[member].name;
+        sandraBio[2].textContent = Team[member].bio;
     }
 
     function parseMariam() {
         
         let firstName = this.alt.split(' '),
-             member = firstName[0];  
-        
-        mariamBio[0].src = `images/${Team[member].avatar}`;
-        mariamBio[1].textContent = Team[member].role;
-        mariamBio[2].textContent = Team[member].name;
-        mariamBio[3].textContent = Team[member].bio;
+            member = firstName[0];  
+        mariamImg.src = "images/mariam_hover3.jpg";
+        this.nextElementSibling.style.display = 'block';
+        // mariamBio[0].src = `images/${Team[member].avatar}`;
+        mariamBio[0].textContent = Team[member].role;
+        mariamBio[1].textContent = Team[member].name;
+        mariamBio[2].textContent = Team[member].bio;
     }
 
     function parseKahani() {
         
         let firstName = this.alt.split(' '),
-             member = firstName[0];  
-        
-        kahaniBio[0].src = `images/${Team[member].avatar}`;
-        kahaniBio[1].textContent = Team[member].role;
-        kahaniBio[2].textContent = Team[member].name;
-        kahaniBio[3].textContent = Team[member].bio;
+            member = firstName[0];  
+        kahaniImg.src = "images/kahani_hover3.jpg";
+        this.nextElementSibling.style.display = 'block';
+        // kahaniBio[0].src = `images/${Team[member].avatar}`;
+        kahaniBio[0].textContent = Team[member].role;
+        kahaniBio[1].textContent = Team[member].name;
+        kahaniBio[2].textContent = Team[member].bio;
     }
 
     function parseLuisa() {
         
         let firstName = this.alt.split(' '),
-             member = firstName[0];  
-        
-        luisaBio[0].src = `images/${Team[member].avatar}`;
-        luisaBio[1].textContent = Team[member].role;
-        luisaBio[2].textContent = Team[member].name;
-        luisaBio[3].textContent = Team[member].bio;
+            member = firstName[0];  
+        luisaImg.src = "images/luisa_hover3.jpg";
+        this.nextElementSibling.style.display = 'block';
+        // luisaBio[0].src = `images/${Team[member].avatar}`;
+        luisaBio[0].textContent = Team[member].role;
+        luisaBio[1].textContent = Team[member].name;
+        luisaBio[2].textContent = Team[member].bio;
     }
 
     
