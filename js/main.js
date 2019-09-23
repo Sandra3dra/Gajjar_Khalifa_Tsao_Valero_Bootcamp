36c21b3402e6099873316bb6db46726f45718fc2
Sandra Tsao <sandratsao@Sandras-MacBook-Pro.local>
2019-09-22 23:35:18 -0400

    js fixed and done

--- js/main.js
+++ js/main.js
@@ -13,7 +13,11 @@ import Team from './modules/dataObject.js';
         sandraBio = document.querySelector('#sandraBio').children,
         mariamBio = document.querySelector('#mariamBio').children,
         kahaniBio = document.querySelector('#kahaniBio').children,
-        luisaBio = document.querySelector('#luisaBio').children;    // console.log(memberBio);
+        luisaBio = document.querySelector('#luisaBio').children,
+        sandraBi = document.querySelector('#sandraBio'),
+        mariamBi = document.querySelector('#mariamBio'),
+        kahaniBi = document.querySelector('#kahaniBio'),
+        luisaBi = document.querySelector('#luisaBio');    // console.log(memberBio);
     // console.log(teamImages);
 
     // function parseMember() {
@@ -31,11 +35,10 @@ import Team from './modules/dataObject.js';
 
     function parseSandra() {
         
-        let firstName = this.alt.split(' '),
-            member = firstName[0];  
+        let member = "sandra"; 
         sandraImg.src = "images/sandra_hover3.jpg";
         // console.log(this.nextElementSibling);
-        this.nextElementSibling.style.display = 'block';
+        sandraBi.style.display = 'block';
         // this.nextSibling.classList.style.display = "block";
         
         // sandraBio[0].src = `images/${Team[member].avatar}`;
@@ -44,40 +47,69 @@ import Team from './modules/dataObject.js';
         sandraBio[2].textContent = Team[member].bio;
     }
 
+    function byeSandra() {
+        
+        // let firstName = this.alt.split(' '),
+        //     member = firstName[0];  
+        sandraImg.src = "images/sandra_pic3.jpg";
+        sandraBi.style.display = 'none';
+    }
+
     function parseMariam() {
         
-        let firstName = this.alt.split(' '),
-            member = firstName[0];  
+        let member = "mariam"; 
         mariamImg.src = "images/mariam_hover3.jpg";
-        this.nextElementSibling.style.display = 'block';
+        mariamBi.style.display = 'block';
         // mariamBio[0].src = `images/${Team[member].avatar}`;
         mariamBio[0].textContent = Team[member].role;
         mariamBio[1].textContent = Team[member].name;
         mariamBio[2].textContent = Team[member].bio;
     }
 
+    function byeMariam() {
+        
+        // let firstName = this.alt.split(' '),
+        //     member = firstName[0];  
+        mariamImg.src = "images/mariam_pic3.jpg";
+        mariamBi.style.display = 'none';
+    }
+
     function parseKahani() {
         
-        let firstName = this.alt.split(' '),
-            member = firstName[0];  
+        let member = "kahani";
         kahaniImg.src = "images/kahani_hover3.jpg";
-        this.nextElementSibling.style.display = 'block';
+        kahaniBi.style.display = 'block';
         // kahaniBio[0].src = `images/${Team[member].avatar}`;
         kahaniBio[0].textContent = Team[member].role;
         kahaniBio[1].textContent = Team[member].name;
         kahaniBio[2].textContent = Team[member].bio;
     }
 
+    function byeKahani() {
+        
+        // let firstName = this.alt.split(' '),
+        //     member = firstName[0];  
+        kahaniImg.src = "images/kahani_pic3.jpg";
+        kahaniBi.style.display = 'none';
+    }
+
     function parseLuisa() {
         
-        let firstName = this.alt.split(' '),
-            member = firstName[0];  
+        let member = "luisa"; 
         luisaImg.src = "images/luisa_hover3.jpg";
-        this.nextElementSibling.style.display = 'block';
+        luisaBi.style.display = 'block';
         // luisaBio[0].src = `images/${Team[member].avatar}`;
         luisaBio[0].textContent = Team[member].role;
         luisaBio[1].textContent = Team[member].name;
         luisaBio[2].textContent = Team[member].bio;
+    }
+
+    function byeLuisa() {
+        
+        // let firstName = this.alt.split(' '),
+        //     member = firstName[0];  
+        luisaImg.src = "images/luisa_pic3.jpg";
+        luisaBi.style.display = 'none';
     }
 
     
@@ -90,5 +122,20 @@ import Team from './modules/dataObject.js';
     mariamImg.addEventListener('mouseover', parseMariam);
     kahaniImg.addEventListener('mouseover', parseKahani);
     luisaImg.addEventListener('mouseover', parseLuisa);
+
+    sandraBi.addEventListener('mouseover', parseSandra);
+    mariamBi.addEventListener('mouseover', parseMariam);
+    kahaniBi.addEventListener('mouseover', parseKahani);
+    luisaBi.addEventListener('mouseover', parseLuisa);
+
+    sandraImg.addEventListener('mouseleave', byeSandra);
+    mariamImg.addEventListener('mouseleave', byeMariam);
+    kahaniImg.addEventListener('mouseleave', byeKahani);
+    luisaImg.addEventListener('mouseleave', byeLuisa);
+
+    sandraBi.addEventListener('mouseleave', byeSandra);
+    mariamBi.addEventListener('mouseleave', byeMariam);
+    kahaniBi.addEventListener('mouseleave', byeKahani);
+    luisaBi.addEventListener('mouseleave', byeLuisa);
 
 })();
